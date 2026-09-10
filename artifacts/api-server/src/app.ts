1432798666404a80cb2ae3c9aeca2701d5b784fa
wasdwasdwasd12340987 <moekofahi1@gmail.com>
2026-09-10 12:08:03 -0500

    Refactor pino-http import to use module syntax

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,9 +1,9 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import { pinoHttp } from "pino-http";
+import * as pinoHttpModule from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
-
+const pinoHttp = (pinoHttpModule as any).default ?? (pinoHttpModule as any).pinoHttp ?? pinoHttpModule;
 const app: Express = express();
 
 app.use(
